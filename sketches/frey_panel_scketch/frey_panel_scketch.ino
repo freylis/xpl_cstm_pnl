63ed62f957a25dc2d85124656f9bf949883f51ff
mn.volkov <mn.volkov@tensor.ru>
2022-01-31 19:26:46 +0500

    Migrate to one callback FULLSTATE

--- sketches/frey_panel_scketch/frey_panel_scketch.ino
+++ sketches/frey_panel_scketch/frey_panel_scketch.ino
@@ -1,17 +1,25 @@
-// welcome to freylis plane panel sketch
+/* 
+ * Welcome to freylis plane panel sketch
+ */
+
+#define EB_HALFSTEP_ENC true;
+
+#include <GyverTM1637.h>
+#include <EncButton.h>
+
 
 
 bool lastGearState = false;
 bool isGearUp = false;
+unsigned int flapsDegrees = 0;
+
 const int PIN_GEAR_DISPLAY = 11;
 const int PIN_GEAR_BUTTON = 10;
-
 
 
 void sendLog(String message) {
   Serial.println("[frey-log-a] " + message);
 }
-
 
 
 void sendCommand(String command) {
@@ -20,7 +28,6 @@ void sendCommand(String command) {
 
 
 void sendSelfCommand(String command) {
-  sendLog("do self command " + command);
   Serial.println("[frey-cmd-a] " + command);
 }
 
@@ -31,11 +38,115 @@ void commandGearUp() {
   digitalWrite(PIN_GEAR_DISPLAY, LOW);
 }
 
+
 void commandGearDown() {
   sendCommand("GEARDOWN");
   isGearUp = false;
   digitalWrite(PIN_GEAR_DISPLAY, HIGH);
 }
+
+
+void handleGearFromFullState(String fullState) {
+  String gearString = fullState.substring(23, 26);
+  if (gearString == "100") {
+    commandGearDown();
+  } else if (gearString == "000") {
+    commandGearUp();
+  } else {
+    sendLog("Unknown gear value=" + gearString + " from fullstate");
+  }
+}
+
+
+void handleSpeedBrakeFromFullState(String fullState) {
+  String sbrake = fullState.substring(28, 29);
+  sendLog("Handle speedbrabkes " + sbrake);
+}
+
+
+void setFlaps(int dFlaps) {
+  flapsDegrees = dFlaps;
+  sendLog("Write " + (String)dFlaps + " in flaps display");
+}
+
+
+void handleFlapsFromFullState(String fullState) {
+  unsigned int rFlaps = fullState.substring(33, 36).toInt();
+  unsigned int dFlaps = 0;
+  /* zibo737 only */
+  switch (rFlaps) {
+    case 0:
+      dFlaps = 0;
+      break;
+    case 12:
+      dFlaps = 1;
+      break;
+    case 25:
+      dFlaps = 2;
+      break;
+    case 37:
+      dFlaps = 5;
+      break;
+    case 50:
+      dFlaps = 10;
+      break;
+    case 62:
+      dFlaps = 15;
+      break;
+    case 75:
+      dFlaps = 25;
+      break;
+    case 87:
+      dFlaps = 30;
+      break;
+    case 40:
+      dFlaps = 100;
+      break;
+    default:
+      dFlaps = -1;
+      break;
+  };
+  if (dFlaps > 0) {
+    setFlaps(dFlaps);
+  } else {
+    sendLog("Unknown rflaps value" + (String)rFlaps);
+  };
+}
+
+
+void setFullState(String fullState) {
+  /* Соглашение следующее
+   * [frey-cmd-x] STATEFULL aaa__bbb__ccc__ddd
+   * 01234567890123456789012345678901234567890123456789
+   * gear - 23..26
+   * flaps - 28..31
+   * speedbrake - 33..36
+   * vert_trim - 38..41
+   */
+   handleGearFromFullState(fullState);
+   handleFlapsFromFullState(fullState);
+   handleSpeedBrakeFromFullState(fullState);
+}
+
+
+void executeCommand(String command) {
+  command.trim();
+  /* 
+   * попытка обработать внешнюю команду STATEFULL 
+   */
+  if (command.startsWith("[frey-cmd-x] STATEFULL ")) {
+      setFullState(command);
+  }
+  else if (command == "[frey-cmd-x] GEARUP") {
+      commandGearUp();
+  }
+  else if (command == "[frey-cmd-x] GEARDOWN") {
+      commandGearDown();
+  } else {
+      sendLog("Unknown command '" + command + "'");
+  }
+}
+
 
 void handleExternalCommand() {
   /*
@@ -83,25 +194,11 @@ void listenMyself() {
 }
 
 
-void executeCommand(String command) {
-  command.trim();
-  if (command == "[frey-cmd-x] GEARUP") {
-      commandGearUp();
-  }
-  else if (command == "[frey-cmd-x] GEARDOWN") {
-      commandGearDown();
-  } else {
-      sendLog("Unknown command '" + command + "'");
-  }
-}
-
-
 void startUp() {
   /*
    * Комплекс мероприятий по подготовке при старте
    */
   preparePins();
-  loadCurrentValues();
 }
 
 
@@ -109,24 +206,64 @@ void preparePins() {
   /*
    * Подготовим пины к работе
    */
-   pinMode(PIN_GEAR_DISPLAY, OUTPUT);
-   pinMode(PIN_GEAR_BUTTON, INPUT_PULLUP);
+   
 }
 
+int pos_encoder = 1; // Первоначальная позиция енкодера равна 0
+int Last;            // Тут всегда будет лежать предыдущие значение положения енкодера 
+int DT;
+boolean left;
+int CLK = 12;
+int DIO = 13;
 
-void loadCurrentValues() {
-}
+const int pin_CLK = 7;
+const int pin_DT = 8;
 
 
-void setup() {
-  Serial.begin(9600);
-  Serial.setTimeout(100);
-  startUp();
+GyverTM1637 disp(CLK, DIO);
+EncButton<EB_TICK, pin_CLK, pin_DT> enc;
+
+
+void setup() { 
+   pinMode (pin_DT, INPUT);
+   pinMode (pin_CLK, INPUT);
+   Last = digitalRead(pin_DT); // Считываем значение на выходе DT и запоминаем его
+   Serial.begin (9600);
+   disp.clear();
+   disp.brightness(5);
+   disp.displayInt(pos_encoder);
 }
 
 void loop() {
-  // проверим наличие внешней команды. Если есть и она хорошая - выполним ее
+  // listen to external command
   handleExternalCommand();
-  // calc myself commands
-  listenMyself();
-}
+
+  enc.tick();
+
+  if (enc.turn()) {
+    if (enc.left()) {
+      if (enc.fast()) {
+        Serial.println("isfast left");
+        pos_encoder -= 10;
+      } else {
+        pos_encoder -= 1;
+      }
+    } else if (enc.right()) {
+      if (enc.fast()) {
+        pos_encoder += 10;
+        Serial.println("isfast right");
+      } else {
+        pos_encoder += 1;
+      }
+    };
+    if (pos_encoder > 360) {
+      pos_encoder = 1;
+    }
+    else if (pos_encoder < 1) {
+      pos_encoder = 360;
+    };
+    disp.displayInt(pos_encoder);
+    Serial.println("write " + (String)pos_encoder);
+  };
+
+} 
