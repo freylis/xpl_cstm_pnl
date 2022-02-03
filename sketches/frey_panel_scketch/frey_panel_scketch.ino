6fa3ad65c0d0e074c3b0bf22b3bbaa67aa1c8322
mn.volkov <mn.volkov@tensor.ru>
2022-02-03 20:17:32 +0500

    Migrate to one callback FULLSTATE

--- sketches/frey_panel_scketch/frey_panel_scketch.ino
+++ sketches/frey_panel_scketch/frey_panel_scketch.ino
@@ -12,23 +12,23 @@
  Define all pin constants
 */
 // gear
-const unsigned int pinGearToggleButtonIn = 53;
-const unsigned int pinGearToggleButtonOut = 52;
+const unsigned int pinGearToggleButtonIn = 2;
+const unsigned int pinGearToggleButtonOut = 3;
 // speed brakes
-const unsigned int pinSpeedBrakesEncoderCLK = 51;
-const unsigned int pinSpeedBrakesEncoderDIO = 50;
-const unsigned int pinSpeedBrakesDisplayCLK = 49;
-const unsigned int pinSpeedBrakesDisplayDIO = 48;
+const unsigned int pinSpeedBrakesEncoderCLK = 4;
+const unsigned int pinSpeedBrakesEncoderDIO = 5;
+const unsigned int pinSpeedBrakesDisplayCLK = 6;
+const unsigned int pinSpeedBrakesDisplayDIO = 7;
 // flaps
-const unsigned int pinFlapsEncoderCLK = 47;
-const unsigned int pinFlapsEncoderDIO = 46;
-const unsigned int pinFlapsDisplayCLK = 45;
-const unsigned int pinFlapsDisplayDIO = 44;
+const unsigned int pinFlapsEncoderCLK = 8;
+const unsigned int pinFlapsEncoderDIO = 9;
+const unsigned int pinFlapsDisplayCLK = 10;
+const unsigned int pinFlapsDisplayDIO = 11;
 // vertical trim
-const unsigned int pinVertTrimEncoderCLK = 43;
-const unsigned int pinVertTrimEncoderDIO = 42;
-const unsigned int pinVertTrimDisplayCLK = 41;
-const unsigned int pinVertTrimDisplayDIO = 40;
+const unsigned int pinVertTrimEncoderCLK = 12;
+const unsigned int pinVertTrimEncoderDIO = 13;
+const unsigned int pinVertTrimDisplayCLK = 12;
+const unsigned int pinVertTrimDisplayDIO = 13;
 
 
 GyverTM1637 displaySpeedBrakes(pinSpeedBrakesDisplayCLK, pinSpeedBrakesDisplayDIO);
@@ -41,6 +41,9 @@ EncButton<EB_TICK, pinVertTrimEncoderCLK, pinVertTrimEncoderDIO> encoderVertTrim
 
 bool lastGearState = false;
 bool isGearUp = false;
+int flapsDegrees = 0;
+int speedBrakeState = 0;
+int verticalTrimValue = 0;
 
 
 void sendLog(String message) {
@@ -61,14 +64,61 @@ void sendSelfCommand(String command) {
 void commandGearUp() {
   sendCommand("GEARUP");
   isGearUp = true;
-  digitalWrite(PIN_GEAR_DISPLAY, LOW);
+  digitalWrite(pinGearToggleButtonOut, LOW);
 }
 
 void commandGearDown() {
   sendCommand("GEARDOWN");
   isGearUp = false;
-  digitalWrite(PIN_GEAR_DISPLAY, HIGH);
+  digitalWrite(pinGearToggleButtonOut, HIGH);
 }
+
+void speedBrakeDown() {
+  displaySpeedBrakes.displayByte(0, _D);
+  displaySpeedBrakes.displayByte(1, _o);
+  displaySpeedBrakes.displayByte(2, _u);
+  displaySpeedBrakes.displayByte(3, _n);
+  speedBrakeState = 0;
+  sendLog("Draw sbrake down");
+}
+
+void speedBrakeArmed() {
+  displaySpeedBrakes.displayByte(0, _empty);
+  displaySpeedBrakes.displayByte(1, _A);
+  displaySpeedBrakes.displayByte(2, _r);
+  displaySpeedBrakes.displayByte(3, _d);
+
+  speedBrakeState = 1;
+  sendLog("Draw sbrake armed");
+}
+void speedBrakeFlightDetent() {
+  displaySpeedBrakes.displayByte(0, _F);
+  displaySpeedBrakes.displayByte(1, _l);
+  displaySpeedBrakes.displayByte(2, _D);
+  displaySpeedBrakes.displayByte(3, _t);
+
+  speedBrakeState = 2;
+  sendLog("Draw sbrake flight detent");
+}
+void speedBrakeUp() {
+  displaySpeedBrakes.displayByte(0, _empty);
+  displaySpeedBrakes.displayByte(1, _empty);
+  displaySpeedBrakes.displayByte(2, _U);
+  displaySpeedBrakes.displayByte(3, _P);
+
+  speedBrakeState = 3;
+  sendLog("Draw sbrake flight up");
+}
+void speedBrakeUnknown() {
+  displaySpeedBrakes.displayByte(0, _empty);
+  displaySpeedBrakes.displayByte(1, _E);
+  displaySpeedBrakes.displayByte(2, _r);
+  displaySpeedBrakes.displayByte(3, _r);
+
+  speedBrakeState = 0;
+  sendLog("Draw sbrake error");
+}
+
 
 
 void handleGearFromFullState(String fullState) {
@@ -84,38 +134,93 @@ void handleGearFromFullState(String fullState) {
 
 
 void handleSpeedBrakeFromFullState(String fullState) {
-  /*
   int rbrake = fullState.substring(28, 31).toInt();
-  byte sbrake[4];
-  sendLog("Handle rspeedbrabkes " + sbrake);
+  sendLog("Handle rspeedbrabkes " + (String)rbrake);
   switch (rbrake) {
     case 0:
-      sbrake = {_D, _o, _u, _n};
+      speedBrakeDown();
       break;
     case -50:
-      sbrake = {_A, _r, _d};
+      speedBrakeArmed();
       break;
     case 98:
-      sbrake = {_F, _l, _D, _t};
+      speedBrakeFlightDetent();
       break;
     case 100:
-      sbrake[0] = _U;
-      sbrake[1] = _P;
+      speedBrakeUp();
       break;
     default:
-      sbrake[0] = _E;
-      sbrake[1] = _r;
-      sbrake[2] = _r;
-      // {_E, _r, _r};
+      speedBrakeUnknown();
       break;
   };
-  */
-  //sendLog("Draw sbrake " + (String)sbrake);
 }
 
 
 void setFlaps(int dFlaps) {
-  flasDegrees = dFlaps;
+  flapsDegrees = dFlaps;
+  switch (dFlaps) {
+    case 0:
+      displayFlaps.displayByte(0, _empty);
+      displayFlaps.displayByte(1, _empty);
+      displayFlaps.displayByte(2, _empty);
+      displayFlaps.display(3, 0);
+      break;
+    case 1:
+      displayFlaps.displayByte(0, _empty);
+      displayFlaps.displayByte(1, _empty);
+      displayFlaps.displayByte(2, _empty);
+      displayFlaps.display(3, 1);
+      break;
+    case 2:
+      displayFlaps.displayByte(0, _empty);
+      displayFlaps.displayByte(1, _empty);
+      displayFlaps.displayByte(2, _empty);
+      displayFlaps.display(3, 2);
+      break;
+    case 5:
+      displayFlaps.displayByte(0, _empty);
+      displayFlaps.displayByte(1, _empty);
+      displayFlaps.displayByte(2, _empty);
+      displayFlaps.display(3, 5);
+      break;
+    case 10:
+      displayFlaps.displayByte(0, _empty);
+      displayFlaps.displayByte(1, _empty);
+      displayFlaps.display(2, 1);
+      displayFlaps.display(3, 0);
+      break;
+    case 15:
+      displayFlaps.displayByte(0, _empty);
+      displayFlaps.displayByte(1, _empty);
+      displayFlaps.display(2, 1);
+      displayFlaps.display(3, 5);
+      break;
+    case 25:
+      displayFlaps.displayByte(0, _empty);
+      displayFlaps.displayByte(1, _empty);
+      displayFlaps.display(2, 2);
+      displayFlaps.display(3, 5);
+      break;
+    case 30:
+      displayFlaps.displayByte(0, _empty);
+      displayFlaps.displayByte(1, _empty);
+      displayFlaps.display(2, 3);
+      displayFlaps.display(3, 0);
+      break;
+    case 40:
+      displayFlaps.displayByte(0, _empty);
+      displayFlaps.displayByte(1, _empty);
+      displayFlaps.display(2, 4);
+      displayFlaps.display(3, 0);
+      break;
+    default:
+      displayFlaps.displayInt(dFlaps);
+      displayFlaps.displayByte(0, _dash);
+      break;
+
+  }
+  
+  
   sendLog("Draw " + (String)dFlaps + " in flaps display");
 }
 
@@ -149,18 +254,81 @@ void handleFlapsFromFullState(String fullState) {
     case 87:
       dFlaps = 30;
       break;
-    case 40:
-      dFlaps = 100;
+    case 100:
+      dFlaps = 40;
       break;
     default:
-      dFlaps = -1;
+      dFlaps = - rFlaps;
       break;
   };
-  if (dFlaps > 0) {
-    setFlaps(dFlaps);
-  } else {
-    sendLog("Unknown rflaps value" + (String)rFlaps);
-  };
+  setFlaps(dFlaps);
+}
+
+
+void setVerticalTrimValue(int iVertTrim) {
+   verticalTrimValue = iVertTrim;
+   String sVertTrim = (String)iVertTrim;
+   sendLog("Draw vtrim " + sVertTrim);
+   if (iVertTrim < -9) {
+      // -63 .. -10
+      displayVertTrim.displayByte(3, _empty);
+      displayVertTrim.displayByte(2, _dash);
+      displayVertTrim.display(1, (int)sVertTrim[1]);
+      displayVertTrim.display(0, (int)sVertTrim[2]);
+      sendLog("D1:" + sVertTrim + "/" + sVertTrim[1] + "/" + sVertTrim[2]);
+
+   } else if (iVertTrim < 0) {
+      // -9..-1
+      displayVertTrim.displayByte(0, _empty);
+      displayVertTrim.displayByte(1, _empty);
+      displayVertTrim.displayByte(2, _dash);
+      displayVertTrim.display(3, (int)sVertTrim[1]);
+      sendLog("D2");
+
+   } else if (iVertTrim == 0) {
+      // 0
+      displayVertTrim.displayByte(0, _empty);
+      displayVertTrim.displayByte(1, _empty);
+      displayVertTrim.displayByte(2, _empty);
+      displayVertTrim.display(3, 0);
+      sendLog("D3");
+
+   } else if (iVertTrim < 10) {
+     // 1..9
+     displayVertTrim.displayByte(0, _empty);
+     displayVertTrim.displayByte(1, _empty);
+     displayVertTrim.displayByte(2, _empty);
+     displayVertTrim.display(3, iVertTrim);
+     sendLog("D4");
+
+   } else if (iVertTrim < 100) {
+     // 11..99
+     displayVertTrim.displayByte(0, _empty);
+     displayVertTrim.displayByte(1, _empty);
+     displayVertTrim.display(2, (int)sVertTrim[0]);
+     displayVertTrim.display(3, (int)sVertTrim[1]);
+     sendLog("D5");
+
+   } else {
+     // 100..999
+     displayVertTrim.displayByte(0, _empty);
+     displayVertTrim.display(1, (int)sVertTrim[0]);
+     displayVertTrim.display(2, (int)sVertTrim[1]);
+     displayVertTrim.display(3, (int)sVertTrim[2]);
+     sendLog("D6");
+   }
+}
+
+
+void handleVerticalTrimFromFullState(String fullState) {
+  /*
+   * [frey-cmd-x] STATEFULL aaa__bbb__ccc__ddd
+   * vertical trim is ddd: 38-41 position
+   */
+   unsigned int iVertTrim = fullState.substring(38, 41).toInt();
+   // из-за дисплея я вынужден рисовать каждый знак отдельно
+   sendLog("Set vtrim " + (String)iVertTrim);
+   setVerticalTrimValue(iVertTrim);
 }
 
 
@@ -169,13 +337,14 @@ void setFullState(String fullState) {
    * [frey-cmd-x] STATEFULL aaa__bbb__ccc__ddd
    * 01234567890123456789012345678901234567890123456789
    * gear - 23..26
-   * flaps - 28..31
-   * speedbrake - 33..36
+   * speedbrake - 28..31
+   * flaps - 33..36
    * vert_trim - 38..41
    */
    handleGearFromFullState(fullState);
    handleFlapsFromFullState(fullState);
    handleSpeedBrakeFromFullState(fullState);
+   handleVerticalTrimFromFullState(fullState);
 }
 
 
@@ -219,7 +388,7 @@ void calcGear() {
   /* 
    *  Выясняем, меняется ли состояние шасси
    */
-   boolean gearState = !digitalRead(PIN_GEAR_BUTTON);
+   boolean gearState = !digitalRead(pinGearToggleButtonIn);
    if (gearState && !lastGearState) {
       lastGearState = true;
       if (isGearUp) {
@@ -277,9 +446,40 @@ void preparePins() {
 }
 
 
+void afterStartCheckList() {
+  sendLog("After start checklist");
+  displaySpeedBrakes.clear();
+  displaySpeedBrakes.brightness(5);
+  displaySpeedBrakes.displayByte(0, _L);
+  displaySpeedBrakes.displayByte(1, _O);
+  displaySpeedBrakes.displayByte(2, _A);
+  displaySpeedBrakes.displayByte(3, _D);
+
+  displayFlaps.clear();
+  displayFlaps.brightness(5);
+  displayFlaps.displayByte(0, _L);
+  displayFlaps.displayByte(1, _O);
+  displayFlaps.displayByte(2, _A);
+  displayFlaps.displayByte(3, _D);
+
+  displayVertTrim.clear();
+  displayVertTrim.brightness(5);
+  displayVertTrim.displayByte(0, _L);
+  displayVertTrim.displayByte(1, _O);
+  displayVertTrim.displayByte(2, _A);
+  displayVertTrim.displayByte(3, _D);
+
+  delay(2000);
+
+  executeCommand("[frey-cmd-x] STATEFULL 100__-50__037__-65");
+  sendLog("After start checklist complete");
+}
+
+
 void setup() { 
   Serial.begin (9600);
   preparePins();
+  afterStartCheckList();
   /*
    * display + encoder
    pinMode (pin_DT, INPUT);
@@ -293,12 +493,11 @@ void setup() {
 }
 
 void loop() {
-  // listen to external command
-  // handleExternalCommand();
+  // listen panel
+  listenMyself();
 
-  int lVal = digitalRead(toggleButtonLeft);
-  int rVal = digitalRead(toggleButtonRight);
-  Serial.println("l: " + (String)lVal + " | " + (String)rVal + " :r");
+  // listen to external command
+  handleExternalCommand();
 
   /*
    * encoder + display
